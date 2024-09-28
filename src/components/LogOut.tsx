36d683eca3c9fefd7758b34fa50e0fea60a29a37
DmdXcode <elijahsolomon003@gmail.com>
2024-09-29 00:32:18 +0100

    new file:   npm 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	deleted:    src/App.test.tsx 	modified:   src/App.tsx 	new file:   src/components/InputField.tsx 	new file:   src/components/LogOut.tsx 	new file:   src/components/TodoList.tsx 	new file:   src/components/styles.css 	modified:   src/index.css 	modified:   src/index.tsx 	deleted:    src/logo.svg 	new file:   src/model.ts 	deleted:    src/reportWebVitals.ts 	deleted:    src/setupTests.ts 	new file:   tailwind.config.js

---
+++ src/components/LogOut.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+function LogOut() {
+  return (
+    <div>
+        <button style={{
+            display:'flex',
+            justifyContent: "space-between"
+        }}>
+          <Btn1>LOG IN</Btn1>
+          <Btn2>REGISTER</Btn2>  
+        </button>      
+    </div>
+  )
+}
+
+export default LogOut
+
+const Btn1 = styled.div`
+wid
+`
+const Btn2 = styled.div`
+
+`
